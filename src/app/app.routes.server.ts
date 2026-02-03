52273e8f6a9666bdab2f7f2b7b358104e48183a9
Renzo Luigi <renzooluigi@hotmail.com>
2026-02-03 14:58:49 -0300

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,31 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import {RenderMode, ServerRoute} from '@angular/ssr';
+import {CHARACTERS} from './characters-data';
 
 export const serverRoutes: ServerRoute[] = [
   {
+
+    path: 'personagens/:codename',
+
+    renderMode: RenderMode.Prerender,
+
+    getPrerenderParams: async () => {
+
+      return CHARACTERS.map(char => ({
+
+        codename: char.codename
+
+      }));
+
+    }
+
+  },
+
+  {
+
     path: '**',
+
     renderMode: RenderMode.Prerender
+
   }
+
 ];
